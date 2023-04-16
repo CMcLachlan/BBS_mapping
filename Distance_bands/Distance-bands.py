cf3336f9f2406f341185febc621ef6091e3a1e94
CMcLachlan <mclachlan-c@ulster.ac.uk>
2023-04-16 20:36:21 +0100

    creation of function for creation of distance bands
    Distance bands creation written into function and tested on 0-25, 25-100 and 100-300m distances.

--- Distance_bands/Distance-bands.py
+++ Distance_bands/Distance-bands.py
@@ -2,42 +2,60 @@ import geopandas as gpd
 
 test_transects = gpd.read_file('Test_data/test_transects.shp')
 
-left_buffer25 = test_transects.buffer(25, single_sided=True)  # create initial 25m left buffer
-LB025gdf = gpd.GeoDataFrame(gpd.GeoSeries(left_buffer25))  # convert buffer to geodataframe to add attributes
-LB025gdf = LB025gdf.rename(columns={0:'geometry'}).set_geometry('geometry')  # set geometry of geodataframe
-LB025join = LB025gdf.sjoin(test_transects, how="inner", predicate='touches')  # join attributes from transects to buffer
-LB025join['Side'] = 'L'  # add column to label these polygons as the "left" recording compartments
-LB025join['Band'] = '0-25'  # add column to label these polygons as the "0-25m" distance band
+def distance_bands(transect, dist_from, dist_to, left=True, right=True):
+    """
+    Create a polygon buffer for a defined distance from a line.
 
-temp_left0 = test_transects.buffer(0, single_sided=True)  # create temporary buffer to subtract from the first
-left_buffer025 = left_buffer25.difference(temp_left0)  # create required polygons by subtracting unrequired area
-leftGDF = gpd.GeoDataFrame(gpd.GeoSeries(left_buffer025))  # convert new geoseries to geodataframe
-leftGDF = leftGDF.rename(columns={0:'geometry'}).set_geometry('geometry')  # set geometry of new geodataframe
-LB025 = LB025join.set_geometry(leftGDF['geometry']) # copy geometry from subtracted buffer to geodataframe to be exported
+    :param transect: shapefile
+        The line dataset to be buffered to create the distance bands
+    :param dist_from: int, float
+        Distance in metres the polygon should begin from the transect line
+    :param dist_to: int, float
+        Distance in metres the polygon should reach from the transect line
+    :param left: bool, optional
+        Default = True - to create a polygon on the "left" side of the transect (positive buffer)
+    :param right: bool, optional
+        Default = True - to create a polygon on the "right" side of the transect (negative buffer)
+    :return: dist_from-dist_to_buffer: a geodataframe containing defined distance polygons and attributes from original line
+    """
+    buffer = {}
+    if left:
+        lb = transect.buffer(dist_to, single_sided=True)  # create initial left buffer from 0 to dist_to
+        lbgdf = gpd.GeoDataFrame(gpd.GeoSeries(lb))  # convert buffer to geodataframe to add attributes
+        lbgdf = lbgdf.rename(columns={0: 'geometry'}).set_geometry('geometry')  # set geometry of geodataframe
+        lbjoin = lbgdf.sjoin(transect, how="inner", predicate='touches')  # join attributes from transects to buffer
+        lbjoin['Side'] = 'L' # add column to label these polygons as the "left" recording compartments
+        lbjoin['Band'] = '{}-{}'.format(dist_from, dist_to)  # add column to label these polygons as "dist_from-dist_to" band
+        templeft = transect.buffer(dist_from, single_sided=True)  # create temporary buffer to subtract from the first
+        leftbuf = lb.difference(templeft)  # create required polygons by subtracting unrequired area
+        leftgdf = gpd.GeoDataFrame(gpd.GeoSeries(leftbuf))  # convert new geoseries to geodataframe
+        leftgdf = leftgdf.rename(columns={0: 'geometry'}).set_geometry('geometry')  # set geometry of new geodataframe
+        leftbuffer = lbjoin.set_geometry(leftgdf['geometry'])  # copy geometry from subtracted buffer to geodataframe to be exported
+    if right:
+        rb = transect.buffer(-abs(dist_to), single_sided=True)  # create initial right buffer from 0 to dist_to
+        rbgdf = gpd.GeoDataFrame(gpd.GeoSeries(rb))  # convert buffer to geodataframe to add attributes
+        rbgdf = rbgdf.rename(columns={0: 'geometry'}).set_geometry('geometry')  # set geometry of geodataframe
+        rbjoin = rbgdf.sjoin(transect, how="inner", predicate='touches')  # join attributes from transects to buffer
+        rbjoin['Side'] = 'R'  # add column to label these polygons as the "right" recording compartments
+        rbjoin['Band'] = '{}-{}'.format(dist_from, dist_to)  # add column to label these polygons as "dist_from-dist_to" band
+        tempright = transect.buffer(-abs(dist_from), single_sided=True)  # create temporary buffer to subtract from the first
+        rightbuf = rb.difference(tempright)  # create required polygons by subtracting unrequired area
+        rightgdf = gpd.GeoDataFrame(gpd.GeoSeries(rightbuf))  # convert new geoseries to geodataframe
+        rightgdf = rightgdf.rename(columns={0: 'geometry'}).set_geometry('geometry')  # set geometry of new geodataframe
+        rightbuffer = rbjoin.set_geometry(rightgdf['geometry'])  # copy geometry from subtracted buffer to geodataframe to be exported
+    if left and right:
+        buffer = gpd.pd.merge(leftbuffer, rightbuffer, how='outer')
+    elif not(left):
+        buffer = rightbuffer
+    else:
+        buffer = leftbuffer
+    return buffer
 
+DB025 = distance_bands(test_transects, 0, 25)
+DB25100 = distance_bands(test_transects, 25, 100)
+DB100300 = distance_bands(test_transects, 100, 300)
 
-right_buffer025 = test_transects.buffer(-25, single_sided=True)
-RB025gdf = gpd.GeoDataFrame(gpd.GeoSeries(right_buffer025))
-RB025gdf = RB025gdf.rename(columns={0:'geometry'}).set_geometry('geometry')
-RB025join = RB025gdf.sjoin(test_transects, how="inner", predicate='touches')
-RB025join['Side'] = 'R'
-RB025join['Band'] = '0-25'
+DistanceBandsa = gpd.pd.merge(DB025, DB25100, how='outer')
+DistanceBands = gpd.pd.merge(DistanceBandsa, DB100300, how='outer')
+DistanceBands.to_file('Test_results/DistanceBands_test.shp')
 
-band025 = gpd.pd.merge(LB025join, RB025join, how='outer')
-
-band025.to_file('Test_results/band025_test.shp')
-
-left_buffer25100 = test_transects.buffer(100, single_sided=True)  # create initial 100m left buffer
-LB025100gdf = gpd.GeoDataFrame(gpd.GeoSeries(left_buffer25100))  # convert buffer to geodataframe to add attributes
-LB025100gdf = LB025100gdf.rename(columns={0:'geometry'}).set_geometry('geometry')  # set geometry of geodataframe
-LB025100join = LB025100gdf.sjoin(test_transects, how="inner", predicate='touches')  # join attributes from transects to buffer
-LB025100join['Side'] = 'L'  # add column to label these polygons as the "left" recording compartments
-LB025100join['Band'] = '25-100'  # add column to label these polygons as the "25-100m" distance band
-
-temp_left25 = test_transects.buffer(25, single_sided=True)  # create temporary buffer to subtract from the first
-left_buffer25100 = left_buffer25100.difference(temp_left25)  # create required polygons by subtracting unrequired area
-left25GDF = gpd.GeoDataFrame(gpd.GeoSeries(left_buffer25100))  # convert new geoseries to geodataframe
-left25GDF = left25GDF.rename(columns={0:'geometry'}).set_geometry('geometry')  # set geometry of new geodataframe
-LB25100 = LB025100join.set_geometry(left25GDF['geometry']) # copy geometry from subtracted buffer to geodataframe to be exported
-
-LB25100.to_file('Test_results/left_buffer25_100_test.shp')
